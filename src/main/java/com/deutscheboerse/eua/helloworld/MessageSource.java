634f0a598b73707b111dd2ceb26cafb7c6d369f7
kubedav <davesade.42@gmail.com>
2016-12-01 09:19:18 +0100

    First commit

---
+++ src/main/java/com/deutscheboerse/eua/helloworld/MessageSource.java
@@ -0,0 +1,9 @@
+package com.deutscheboerse.eua.helloworld;
+
+public class MessageSource {
+
+    public String getMessage() {
+        return "Always bear in mind that your own resolution to succeed is more important than any other.";
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page import="com.deutscheboerse.eua.helloworld.MessageSource" %>
+<html>
+<body>
+<h2>Hello World from M7!</h2>
+<p><%=new MessageSource().getMessage()%></p>
+</body>
+</html>
